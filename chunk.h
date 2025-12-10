82eef1f44ce8c27ad0b38aa6681313047569a989
zhaowen <2872186458@qq.com>
2025-12-10 17:26:29 +0800

    支持显示指令码

--- chunk.h
+++ chunk.h
@@ -2,10 +2,22 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "memory.h"
 
 typedef enum
 {
     OP_RETURN,
 } OpCode;
 
+typedef struct
+{
+    // 新增部分开始
+    int count;
+    int capacity;
+    // 新增部分结束
+    uint8_t *code;
+} Chunk;
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
 #endif
